b25839616ad466ee8694308e1c681ab6fd92123d
KvlcmHlHl <125590475+KvlcmHlHl@users.noreply.github.com>
2023-03-14 12:26:20 +0300

    Delete src directory

--- src/main/java/com/example/filmapplication/model/RatingModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.filmapplication.model
-
-import com.google.gson.annotations.SerializedName
-
-data class RatingModel (
-    @SerializedName("Source" ) var Source : String? = null,
-    @SerializedName("Value"  ) var Value  : String? = null
-
-)
